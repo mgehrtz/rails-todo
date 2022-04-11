1cd9d76d27e4304ec67b90c97f307e0f39a4d710
Michael Gehrtz <63627744+mgehrtz@users.noreply.github.com>
2022-04-10 21:26:38 -0500

    Working version.

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -3,14 +3,58 @@ import $ from 'jquery';
 import {
     indexTasks,
     postTask,
+    changeStatus,
+    deleteTask
 } from './requests.js';
 
 indexTasks(function (response) {
     var htmlString = response.tasks.map(function(task) {
-        return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'>\
-                    " + task.content + "\
+        return "<div class='col-12 mb-3 task d-flex align-items-center' data-id='" + task.id + "' data-completed='" + task.completed + "'>\
+                    <span>" + task.content + "</span>\
+                    <button class='btn completed_btn' data-action='mark_complete'><i class='fa-solid fa-check'></i></button>\
+                    <button class='btn active_btn' data-action='mark_active'><i class='fa-solid fa-rotate-left'></i></button>\
+                    <button class='btn delete_btn' data-action='delete'><i class='fa-solid fa-xmark'></i></button>\
                 </div>";
     });
 
     $('#tasks').html(htmlString);
+});
+
+$(document).on('click', 'button:not([type=submit])', function(e) {
+
+    var action = $(e.currentTarget).data('action');
+    var target = e.currentTarget.closest('.task');
+    var id  = $(target).data('id');
+
+    if (action == 'mark_complete' || action == 'mark_active') {
+        changeStatus(id, action, function() {
+            var status = $(target).data('completed');
+            $(target).attr('data-completed', !status);
+        });
+    } else if (action == 'delete') {
+        deleteTask(id, function() {
+            $(target).fadeOut(500);
+            setTimeout(() => $(target).detach(), 500);
+        });
+    }
+
+});
+
+$(document).on('submit', 'form', function(e) {
+    console.log('test');
+    e.preventDefault();
+
+    var content = $('#addTask').val();
+
+    postTask(content, function(response) {
+        console.log(response);
+        $('#tasks').append("<div class='col-12 mb-3 task d-flex align-items-center' data-id='" + response.task.id + "' data-completed='" + response.task.completed + "'>\
+                                <span>" + response.task.content + "</span>\
+                                <button class='btn completed_btn' data-action='mark_complete'><i class='fa-solid fa-check'></i></button>\
+                                <button class='btn active_btn' data-action='mark_active'><i class='fa-solid fa-rotate-left'></i></button>\
+                                <button class='btn delete_btn' data-action='delete'><i class='fa-solid fa-xmark'></i></button>\
+                            </div>");
+    });
+
+    $('#addTask').val('');
 });
--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -32,3 +32,25 @@ export var postTask = function (content, successCB, errorCB) {
   
     $.ajax(request);
   };
+
+  export var changeStatus = function (taskID, action, successCB, errorCB) {
+    var request = {
+      type: 'PUT',
+      url: `api/tasks/${taskID}/${action}?api_key=1`,
+      success: successCB,
+      error: errorCB
+    }
+
+    $.ajax(request);
+  };
+
+  export var deleteTask = function(taskID, successCB, errorCB) {
+    var request = {
+      type: 'DELETE',
+      url: `api/tasks/${taskID}?api_key=1`,
+      success: successCB,
+      error: errorCB
+    }
+
+    $.ajax(request);
+  };
