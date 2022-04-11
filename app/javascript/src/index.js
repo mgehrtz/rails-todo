dcb0a7ce87df01440b3548650a20b5867c303a03
Michael Gehrtz <63627744+mgehrtz@users.noreply.github.com>
2022-04-10 22:43:04 -0500

    Fixed bug with updating task and DOM display.

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -9,7 +9,14 @@ import {
 
 indexTasks(function (response) {
     var htmlString = response.tasks.map(function(task) {
-        return "<div class='col-12 mb-3 task d-flex align-items-center' data-id='" + task.id + "' data-completed='" + task.completed + "'>\
+
+        var completedClass = '';
+        
+        if (task.completed) {
+            completedClass = 'completed';
+        }
+
+        return "<div class='col-12 mb-3 task d-flex align-items-center " + completedClass + "' data-id='" + task.id + "'>\
                     <span>" + task.content + "</span>\
                     <button class='btn completed_btn' data-action='mark_complete'><i class='fa-solid fa-check'></i></button>\
                     <button class='btn active_btn' data-action='mark_active'><i class='fa-solid fa-rotate-left'></i></button>\
@@ -20,7 +27,7 @@ indexTasks(function (response) {
     $('#tasks').html(htmlString);
 });
 
-$(document).on('click', 'button:not([type=submit])', function(e) {
+$(document).on('click', 'button', function(e) {
 
     var action = $(e.currentTarget).data('action');
     var target = e.currentTarget.closest('.task');
@@ -28,8 +35,7 @@ $(document).on('click', 'button:not([type=submit])', function(e) {
 
     if (action == 'mark_complete' || action == 'mark_active') {
         changeStatus(id, action, function() {
-            var status = $(target).data('completed');
-            $(target).attr('data-completed', !status);
+            $(target).toggleClass('completed');
         });
     } else if (action == 'delete') {
         deleteTask(id, function() {
@@ -41,14 +47,20 @@ $(document).on('click', 'button:not([type=submit])', function(e) {
 });
 
 $(document).on('submit', 'form', function(e) {
-    console.log('test');
+
     e.preventDefault();
 
     var content = $('#addTask').val();
 
     postTask(content, function(response) {
-        console.log(response);
-        $('#tasks').append("<div class='col-12 mb-3 task d-flex align-items-center' data-id='" + response.task.id + "' data-completed='" + response.task.completed + "'>\
+
+        var completedClass = '';
+        
+        if (task.completed) {
+            completedClass = 'completed';
+        }
+
+        $('#tasks').append("<div class='col-12 mb-3 task d-flex align-items-center " + completedClass + "' data-id='" + response.task.id + "'>\
                                 <span>" + response.task.content + "</span>\
                                 <button class='btn completed_btn' data-action='mark_complete'><i class='fa-solid fa-check'></i></button>\
                                 <button class='btn active_btn' data-action='mark_active'><i class='fa-solid fa-rotate-left'></i></button>\
